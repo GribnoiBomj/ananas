c307a3c71b3a4bd947a5dfad1d8d10a36ad3548d
GribnoiBomj <116429586+GribnoiBomj@users.noreply.github.com>
2022-10-22 12:48:04 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string name;
+            System.Console.WriteLine("Привет, Яндекс!: ");
+            name = System.Console.ReadLine();
+            System.Console.WriteLine("Приятно познакомиться, " + name + "!");
+        }
+    }
+}
